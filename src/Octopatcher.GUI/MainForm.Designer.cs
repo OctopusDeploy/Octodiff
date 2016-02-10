d141d16a71a07999ebd841e1dc4fd092f8783fda
0xFireball <0xFireball@WindowsBuildServer.local>
2016-02-10 11:08:18 -0800

    OctoPatcher stub

---
+++ src/Octopatcher.GUI/MainForm.Designer.cs
@@ -0,0 +1,48 @@
+﻿
+namespace Octopatcher.GUI
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.SuspendLayout();
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+			this.ClientSize = new System.Drawing.Size(376, 266);
+			this.DisplayHeader = false;
+			this.DisplayTitle = true;
+			this.Name = "MainForm";
+			this.Padding = new System.Windows.Forms.Padding(20, 30, 20, 20);
+			this.Text = "OctoPatcher";
+			this.ResumeLayout(false);
+
+		}
+	}
+}
---
+++ src/Octopatcher.GUI/MainForm.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+using Octodiff.CommandLine;
+
+namespace Octopatcher.GUI
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : MetroForm
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
