3ea052ac720db286e79a074bedcf877b5ac06b42
Mark Rydstrom <mark.rydstrom@octopus.com>
2016-09-07 13:10:41 +1000

    Octodiff building netcoreapp

--- source/Octodiff/CommandLine/Support/CommandLocator.cs
+++ source/Octodiff/CommandLine/Support/CommandLocator.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 
 namespace Octodiff.CommandLine.Support
 {
@@ -8,35 +10,48 @@ namespace Octodiff.CommandLine.Support
         public ICommandMetadata[] List()
         {
             return
-                (from t in typeof (CommandLocator).Assembly.GetTypes()
-                 where typeof (ICommand).IsAssignableFrom(t)
-                 let attribute = (ICommandMetadata) t.GetCustomAttributes(typeof (CommandAttribute), true).FirstOrDefault()
-                 where attribute != null
-                 select attribute).ToArray();
+            (from t in assemblyCommands
+             let attribute = GetCommandAttribute(t)
+             where attribute != null
+             select attribute).ToArray();
         }
 
         public ICommandMetadata Find(string name)
         {
             name = name.Trim().ToLowerInvariant();
-            return (from t in typeof (CommandLocator).Assembly.GetTypes()
-                    where typeof (ICommand).IsAssignableFrom(t)
-                    let attribute = (ICommandMetadata) t.GetCustomAttributes(typeof (CommandAttribute), true).FirstOrDefault()
-                    where attribute != null
-                    where attribute.Name == name || attribute.Aliases.Any(a => a == name)
-                    select attribute).FirstOrDefault();
+            return (from t in assemblyCommands
+                let attribute = GetCommandAttribute(t)
+                where attribute != null
+                where attribute.Name == name || attribute.Aliases.Any(a => a == name)
+                select attribute).FirstOrDefault();
         }
 
         public ICommand Create(ICommandMetadata metadata)
         {
             var name = metadata.Name;
-            var found = (from t in typeof(CommandLocator).Assembly.GetTypes()
-                         where typeof(ICommand).IsAssignableFrom(t)
-                         let attribute = (ICommandMetadata)t.GetCustomAttributes(typeof(CommandAttribute), true).FirstOrDefault()
-                         where attribute != null
-                         where attribute.Name == name || attribute.Aliases.Any(a => a == name)
-                         select t).FirstOrDefault();
+            var found = (from t in assemblyCommands
+                let attribute = GetCommandAttribute(t)
+                where attribute != null
+                where attribute.Name == name || attribute.Aliases.Any(a => a == name)
+                select t).FirstOrDefault();
 
-            return found == null ? null : (ICommand)Activator.CreateInstance(found);
+            return found == null ? null : (ICommand) Activator.CreateInstance(found);
+        }
+
+        IEnumerable<Type> assemblyCommands =>
+#if NET40
+            typeof(CommandLocator).Assembly.GetTypes().Where(t => typeof(ICommand).IsAssignableFrom(t));
+#else
+            typeof(CommandLocator).GetTypeInfo().Assembly.GetTypes().Where(t => typeof(ICommand).GetTypeInfo().IsAssignableFrom(t));
+#endif
+
+        ICommandMetadata GetCommandAttribute(Type type)
+        {
+#if NET40
+            return (ICommandMetadata)type.GetCustomAttributes(typeof(CommandAttribute), true).FirstOrDefault();
+#else
+            return (ICommandMetadata)type.GetTypeInfo().GetCustomAttributes(typeof(CommandAttribute), true).FirstOrDefault();
+#endif
         }
     }
 }
--- source/Octodiff/Core/AggregateCopyOperationsDecorator.cs
+++ source/Octodiff/Core/AggregateCopyOperationsDecorator.cs
@@ -1,5 +1,4 @@
 using System.IO;
-using System.Security.Policy;
 
 namespace Octodiff.Core
 {
