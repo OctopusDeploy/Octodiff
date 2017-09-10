f32ee9fb58421ee18e4824d4c4018133b6ce7c79
Henrik Andersson <henrik.a.andersson@outlook.com>
2017-09-10 17:24:15 +1000

    Update :allthethings: to .NET Core 2.0

--- source/Octodiff/Properties/AssemblyInfo.cs
+++ source/Octodiff/Properties/AssemblyInfo.cs
@@ -3,12 +3,9 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Octodiff")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Octodiff")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyDescription("100% C# implementation of remote delta compression based on the rsync algorithm")]
+[assembly: AssemblyCompany("Octopus Deploy")]
+[assembly: AssemblyProduct("Octodiff, a 100% C# implementation of remote delta compression based on the rsync algorithm")]
+[assembly: AssemblyCopyright("Copyright © Octopus Deploy 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
