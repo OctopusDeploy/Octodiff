260660dc810b82a3f2ad403948e3958ab17a2fcc
Henrik Andersson <henrik.a.andersson@outlook.com>
2017-09-12 14:31:39 +1000

    Project changes

--- source/Octodiff/Properties/AssemblyInfo.cs
+++ source/Octodiff/Properties/AssemblyInfo.cs
@@ -1,11 +1,3 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Octodiff")]
-[assembly: AssemblyDescription("100% C# implementation of remote delta compression based on the rsync algorithm")]
-[assembly: AssemblyCompany("Octopus Deploy")]
-[assembly: AssemblyProduct("Octodiff, a 100% C# implementation of remote delta compression based on the rsync algorithm")]
-[assembly: AssemblyCopyright("Copyright © Octopus Deploy 2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
