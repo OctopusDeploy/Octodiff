be521c3812b10faba83f160460bf31e6d0b026d0
Henrik <henrik.andersson@octopus.com>
2017-10-05 07:27:55 +1000

    Make the SignatureWriter class public. Resolves OctopusDeploy/Octodiff#15

--- source/Octodiff/Core/ISignatureWriter.cs
+++ source/Octodiff/Core/ISignatureWriter.cs
@@ -1,36 +1,8 @@
-using System.IO;
-
 namespace Octodiff.Core
 {
     public interface ISignatureWriter
     {
         void WriteMetadata(IHashAlgorithm hashAlgorithm, IRollingChecksum rollingChecksumAlgorithm);
         void WriteChunk(ChunkSignature signature);
-    }
-
-    class SignatureWriter : ISignatureWriter
-    {
-        private readonly BinaryWriter signatureStream;
-
-        public SignatureWriter(Stream signatureStream)
-        {
-            this.signatureStream = new BinaryWriter(signatureStream);
-        }
-
-        public void WriteMetadata(IHashAlgorithm hashAlgorithm, IRollingChecksum rollingChecksumAlgorithm)
-        {
-            signatureStream.Write(BinaryFormat.SignatureHeader);
-            signatureStream.Write(BinaryFormat.Version);
-            signatureStream.Write(hashAlgorithm.Name);
-            signatureStream.Write(rollingChecksumAlgorithm.Name);
-            signatureStream.Write(BinaryFormat.EndOfMetadata);
-        }
-
-        public void WriteChunk(ChunkSignature signature)
-        {
-            signatureStream.Write(signature.Length);
-            signatureStream.Write(signature.RollingChecksum);
-            signatureStream.Write(signature.Hash);
-        }
     }
 }
