7802103a2c4cda3fecfc7877040a377dbfb3a4b5
Adam McCoy <adam.mccoy@octopus.com>
2022-10-21 14:17:31 +1100

    Add benchmarks

---
+++ source/Octodiff.Benchmarks/DeltaApplierBenchmarks.cs
@@ -0,0 +1,51 @@
+﻿using BenchmarkDotNet.Attributes;
+using Octodiff.Core;
+using Octodiff.Diagnostics;
+
+namespace Octodiff.Benchmarks;
+
+[MemoryDiagnoser]
+public class DeltaApplierBenchmarks
+{
+    private const int _500MB = 0x320_0000;
+    private Stream? deltaStream;
+    private Stream? otherDeltaStream;
+
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        var originalStream = new RandomDataGeneratorStream(_500MB, 100);
+        var newStream = new RandomDataGeneratorStream(_500MB, 200);
+        var signatureStream = new MemoryStream();
+        var signatureBuilder = new SignatureBuilder();
+        signatureBuilder.Build(originalStream, new SignatureWriter(signatureStream));
+
+        signatureStream.Seek(0, SeekOrigin.Begin);
+        var deltaBuilder = new DeltaBuilder();
+        deltaStream = new MemoryStream();
+        deltaBuilder.BuildDelta(newStream, new SignatureReader(signatureStream, new NullProgressReporter()), new BinaryDeltaWriter(deltaStream));
+        deltaStream.Seek(0, SeekOrigin.Begin);
+
+        originalStream.Seek(0, SeekOrigin.Begin);
+        signatureStream.Seek(0, SeekOrigin.Begin);
+        otherDeltaStream = new MemoryStream();
+        deltaBuilder.BuildDelta(originalStream, new SignatureReader(signatureStream, new NullProgressReporter()), new BinaryDeltaWriter(otherDeltaStream));
+        otherDeltaStream.Seek(0, SeekOrigin.Begin);
+    }
+
+    [Benchmark]
+    public void ApplyBigDelta_Different()
+    {
+        var originalStream = new RandomDataGeneratorStream(_500MB, 100);
+        var deltaApplier = new DeltaApplier { SkipHashCheck = true };
+        deltaApplier.Apply(originalStream, new BinaryDeltaReader(deltaStream, new NullProgressReporter()), Stream.Null);
+    }
+
+    [Benchmark]
+    public void ApplyBigDelta_Identical()
+    {
+        var originalStream = new RandomDataGeneratorStream(_500MB, 100);
+        var deltaApplier = new DeltaApplier { SkipHashCheck = true };
+        deltaApplier.Apply(originalStream, new BinaryDeltaReader(otherDeltaStream, new NullProgressReporter()), Stream.Null);
+    }
+}
---
+++ source/Octodiff.Benchmarks/RandomDataGeneratorStream.cs
@@ -0,0 +1,68 @@
+﻿namespace Octodiff.Benchmarks;
+
+public class RandomDataGeneratorStream : Stream
+{
+    private readonly byte[] masterBuffer = new byte[128];
+    private long position;
+
+    public RandomDataGeneratorStream(long desiredLength, int? seed = null)
+    {
+        Length = desiredLength;
+        var random = new Random(seed ?? (int)DateTime.Now.Ticks);
+        random.NextBytes(masterBuffer);
+    }
+
+    public override void Flush()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override int Read(byte[] buffer, int offset, int count)
+    {
+        var toCopy = Math.Min(count, Length - position);
+        var copied = 0;
+        while (copied < toCopy)
+        {
+            var startingPos = (int)(position % masterBuffer.Length);
+            var copyThisTime = (int)Math.Min(masterBuffer.Length - startingPos, toCopy - copied);
+            Buffer.BlockCopy(masterBuffer, startingPos, buffer, offset, copyThisTime);
+            offset += copyThisTime;
+            position += copyThisTime;
+            copied += copyThisTime;
+        }
+        return (int)toCopy;
+    }
+
+    public override long Seek(long offset, SeekOrigin origin)
+    {
+        position = origin switch
+        {
+            SeekOrigin.Begin => offset,
+            SeekOrigin.Current => position + offset,
+            SeekOrigin.End => Length - offset,
+            _ => throw new ArgumentOutOfRangeException(nameof(origin), origin, null)
+        };
+        return position;
+    }
+
+    public override void SetLength(long value)
+    {
+        throw new NotSupportedException();
+    }
+
+    public override void Write(byte[] buffer, int offset, int count)
+    {
+        throw new NotSupportedException();
+    }
+
+    public override bool CanRead => true;
+    public override bool CanSeek => true;
+    public override bool CanWrite => false;
+    public override long Length { get; }
+
+    public override long Position
+    {
+        get => position;
+        set => position = value;
+    }
+}
